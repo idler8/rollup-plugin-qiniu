1623e3e45458fdb11b2e0d3252bafc6b1073b94c
潘丽敏 <code@idler8.com>
2019-10-29 16:35:41 +0800

    增加生成临时zip文件，适应archiver

--- dist/rollup-plugin-qiniu.cjs.js
+++ dist/rollup-plugin-qiniu.cjs.js
@@ -6,6 +6,7 @@ var archiver = _interopDefault(require('archiver'));
 var qiniuModule = _interopDefault(require('qiniu'));
 var globby = _interopDefault(require('globby'));
 var path = _interopDefault(require('path'));
+var fs = _interopDefault(require('fs-extra'));
 
 var config = new qiniuModule.conf.Config();
 var Uploader = new qiniuModule.resume_up.ResumeUploader(config);
@@ -15,48 +16,40 @@ function getToken(key, secret, bucket) {
 	let token = new qiniuModule.rs.PutPolicy({ scope: bucket, expires: 300, insertOnly: 1 }).uploadToken(mac);
 	return token;
 }
-function qiniuStream(token, dest, stream) {
-	return new Promise((resolve, reject) => {
-		Uploader.putStream(token, dest, stream, stream.pointer(), null, (err, ret, info) => {
-			if (err) {
-				console.log(err, info);
-				reject(err);
-			} else {
-				if (ret.hash) console.log('七牛上传成功：KEY=' + dest + ',HASH=' + ret.hash);
-				if (ret.error) console.log('七牛上传失败：KEY=' + dest + ',Error=' + ret.error);
-				resolve(ret);
-			}
-		});
-	});
-}
 function qiniuUpload(token, dest, file) {
 	return new Promise((resolve, reject) => {
 		Uploader.putFile(token, dest, file, null, (err, ret, info) => {
 			if (err) {
-				console.log(err, info);
+				console.error(err, info);
 				reject(err);
 			} else {
-				if (ret.hash) console.log('七牛上传成功：KEY=' + dest + ',HASH=' + ret.hash);
-				if (ret.error) console.log('七牛上传失败：KEY=' + dest + ',Error=' + ret.error);
 				resolve(ret);
 			}
 		});
 	});
 }
 function zipTarget(paths, callback, dir = '') {
-	let zipfile = archiver('zip', { zlib: { store: true } });
-	paths.forEach(src => zipfile.file(dir + src, { name: src }));
-	callback && callback(zipfile);
-	if (!zipfile.close) {
-		zipfile.close = function() {
-			zipfile.destroy();
-		};
-	}
-	return zipfile.finalize().then(() => zipfile);
+	return new Promise((reslove, reject) => {
+		let tmpPath = path.resolve(__dirname, '..', 'tmp');
+		fs.emptyDirSync(tmpPath);
+		let zipfile = archiver('zip', { zlib: { store: true } });
+		let zipPath = path.resolve(tmpPath, Date.now() + '.zip');
+		let output = fs.createWriteStream(zipPath);
+		output.on('close', () => {
+			reslove(zipPath);
+		});
+		output.on('error', err => {
+			reject(err);
+		});
+		paths.forEach(src => zipfile.file(dir + src, { name: src }));
+		callback && callback(zipfile);
+		zipfile.pipe(output);
+		zipfile.finalize();
+	});
 }
 
 function qiniu(options = {}) {
-	let { token, targets, hook = 'buildEnd' } = options;
+	let { token, targets, hook = 'buildEnd', debug = true } = options;
 	if (typeof token == 'function') token = token(getToken);
 
 	return {
@@ -66,7 +59,13 @@ function qiniu(options = {}) {
 			let promises = targets.map(({ root, src, dest, zip }) => {
 				return globby(src, { cwd: root || process.cwd() }).then(paths => {
 					if (zip) {
-						return zipTarget(paths, typeof zip == 'function' ? zip : null, root || '').then(stream => qiniuStream(token, dest, stream));
+						return zipTarget(paths, typeof zip == 'function' ? zip : null, root || '').then(file => {
+							return qiniuUpload(token, dest, file).then(ret => {
+								if (debug && ret.hash) console.log('七牛上传成功：KEY=' + dest + ',HASH=' + ret.hash);
+								if (debug && ret.error) console.log('七牛上传失败：KEY=' + dest + ',Error=' + ret.error);
+								return fs.remove(file);
+							});
+						});
 					} else {
 						return Promise.all(paths.map(file => qiniuUpload(token, dest + file, path.resolve(file))));
 					}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import archiver from 'archiver';
 import qiniuModule from 'qiniu';
 import globby from 'globby';
 import path from 'path';
+import fs from 'fs-extra';
 
 var config = new qiniuModule.conf.Config();
 var Uploader = new qiniuModule.resume_up.ResumeUploader(config);
@@ -15,12 +16,12 @@ function qiniuStream(token, dest, stream) {
 	return new Promise((resolve, reject) => {
 		Uploader.putStream(token, dest, stream, stream.pointer(), null, (err, ret, info) => {
 			if (err) {
-				console.log(err, info);
-				reject(err);
+				console.error(err, info);
+				reject();
 			} else {
 				if (ret.hash) console.log('七牛上传成功：KEY=' + dest + ',HASH=' + ret.hash);
 				if (ret.error) console.log('七牛上传失败：KEY=' + dest + ',Error=' + ret.error);
-				resolve(ret);
+				resolve();
 			}
 		});
 	});
@@ -29,30 +30,36 @@ function qiniuUpload(token, dest, file) {
 	return new Promise((resolve, reject) => {
 		Uploader.putFile(token, dest, file, null, (err, ret, info) => {
 			if (err) {
-				console.log(err, info);
+				console.error(err, info);
 				reject(err);
 			} else {
-				if (ret.hash) console.log('七牛上传成功：KEY=' + dest + ',HASH=' + ret.hash);
-				if (ret.error) console.log('七牛上传失败：KEY=' + dest + ',Error=' + ret.error);
 				resolve(ret);
 			}
 		});
 	});
 }
 function zipTarget(paths, callback, dir = '') {
-	let zipfile = archiver('zip', { zlib: { store: true } });
-	paths.forEach(src => zipfile.file(dir + src, { name: src }));
-	callback && callback(zipfile);
-	if (!zipfile.close) {
-		zipfile.close = function() {
-			zipfile.destroy();
-		};
-	}
-	return zipfile.finalize().then(() => zipfile);
+	return new Promise((reslove, reject) => {
+		let tmpPath = path.resolve(__dirname, '..', 'tmp');
+		fs.emptyDirSync(tmpPath);
+		let zipfile = archiver('zip', { zlib: { store: true } });
+		let zipPath = path.resolve(tmpPath, Date.now() + '.zip');
+		let output = fs.createWriteStream(zipPath);
+		output.on('close', () => {
+			reslove(zipPath);
+		});
+		output.on('error', err => {
+			reject(err);
+		});
+		paths.forEach(src => zipfile.file(dir + src, { name: src }));
+		callback && callback(zipfile);
+		zipfile.pipe(output);
+		zipfile.finalize();
+	});
 }
 
 export default function qiniu(options = {}) {
-	let { token, targets, hook = 'buildEnd' } = options;
+	let { token, targets, hook = 'buildEnd', debug = true } = options;
 	if (typeof token == 'function') token = token(getToken);
 
 	return {
@@ -62,7 +69,13 @@ export default function qiniu(options = {}) {
 			let promises = targets.map(({ root, src, dest, zip }) => {
 				return globby(src, { cwd: root || process.cwd() }).then(paths => {
 					if (zip) {
-						return zipTarget(paths, typeof zip == 'function' ? zip : null, root || '').then(stream => qiniuStream(token, dest, stream));
+						return zipTarget(paths, typeof zip == 'function' ? zip : null, root || '').then(file => {
+							return qiniuUpload(token, dest, file).then(ret => {
+								if (debug && ret.hash) console.log('七牛上传成功：KEY=' + dest + ',HASH=' + ret.hash);
+								if (debug && ret.error) console.log('七牛上传失败：KEY=' + dest + ',Error=' + ret.error);
+								return fs.remove(file);
+							});
+						});
 					} else {
 						return Promise.all(paths.map(file => qiniuUpload(token, dest + file, path.resolve(file))));
 					}
